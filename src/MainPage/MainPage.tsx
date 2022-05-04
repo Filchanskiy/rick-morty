b0a5f9852e77ca8cf9d4b69b965246ea6e9d5975
Pavel <playwikki97@gmail.com>
2022-05-04 15:46:27 +0300

    Fixed function (added output)

--- src/MainPage/MainPage.tsx
+++ src/MainPage/MainPage.tsx
@@ -1,7 +1,15 @@
+import { Container } from '@mui/material';
+import React from 'react';
 import './MainPage.css';
+import Header from './components/Header';
+import Content from './components/Content';
 
 function MainPage() {
-  return (<p>Hello, Pasha</p>);
+  return (
+    <Container>
+      <Header />
+      <Content />
+    </Container>
+  );
 }
-
 export default MainPage;
