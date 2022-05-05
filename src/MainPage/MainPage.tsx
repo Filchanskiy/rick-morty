4a12786d431fca2e916539b2179f8f5b6f788ef4
Pavel <playwikki97@gmail.com>
2022-05-05 13:33:35 +0300

    Added a new component

--- src/MainPage/MainPage.tsx
+++ src/MainPage/MainPage.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 import './MainPage.css';
 import Header from './components/Header';
 import Content from './components/Content';
+import Footer from './components/Footer';
 
 function MainPage() {
   return (
     <Container>
       <Header />
       <Content />
+      <Footer />
     </Container>
   );
 }
--- src/MainPage/components/Footer.tsx
+++ src/MainPage/components/Footer.tsx
@@ -1,6 +1,23 @@
+import { Grid, Typography } from '@mui/material';
 import React from 'react';
 
 function Footer() {
-  return <div />;
+  return (
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      mt={10}
+      mb={5}
+    >
+      <Grid item xs={3}>
+        <Typography variant="h6" component="p">
+          by Pavel Filchenkov 2022
+        </Typography>
+      </Grid>
+    </Grid>
+  );
 }
 export default Footer;
