7a164435f61f5472abbc7c2d494d0e54fb7680e4
Pavel <playwikki97@gmail.com>
2022-05-05 13:34:53 +0300

    Buttons redone + Bugs fixed

--- src/MainPage/components/Header.tsx
+++ src/MainPage/components/Header.tsx
@@ -6,51 +6,63 @@ import {
   Button,
   Typography,
   Grid,
+  Stack,
 } from '@mui/material';
 import React from 'react';
 import HomeIcon from '@mui/icons-material/Home';
 
 function Header() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+    <Box>
+      <AppBar>
         <Toolbar>
           <IconButton
             size="large"
             edge="start"
             color="inherit"
             aria-label="home"
-            sx={{
-              mr: 2,
-            }}
           >
             <HomeIcon />
           </IconButton>
-          <Button variant="text" color="inherit">
-            Docs
-          </Button>
-          <Button variant="text" color="inherit">
-            About
-          </Button>
-          <Button variant="text" color="inherit">
-            SUPPORT US
-          </Button>
+          <Typography
+            alignItems="center"
+            justifyContent="center"
+            flexGrow={1}
+            variant="h6"
+            component="span"
+          >
+            Welcome to our page
+          </Typography>
+          <Stack spacing={2} direction="row">
+            <Button variant="text" color="inherit">
+              Docs
+            </Button>
+            <Button variant="text" color="inherit">
+              About
+            </Button>
+            <Button variant="outlined" color="inherit">
+              Support us
+            </Button>
+          </Stack>
         </Toolbar>
       </AppBar>
-      <Grid
-        container
-        spacing={0}
-        direction="column"
-        alignItems="center"
-        justifyContent="center"
-        style={{ minHeight: '100vh' }}
-      >
-        <Grid item xs={3}>
-          <Typography variant="h1" component="span">
-            The Rick and Morty API
-          </Typography>
+      <Box>
+        <Grid
+          container
+          spacing={0}
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+          pt={20}
+          pb={10}
+        >
+          <Grid item xs={3}>
+            <Typography variant="h1" component="span">
+              The Rick and Morty API
+            </Typography>
+          </Grid>
         </Grid>
-      </Grid>
+      </Box>
     </Box>
   );
 }
