d7be3637f68939acc968ea84d83a14a0f67301db
Pavel <playwikki97@gmail.com>
2022-05-12 13:16:52 +0300

    Redesigned MainPage.tsx file

--- src/MainPage/MainPage.tsx
+++ src/MainPage/MainPage.tsx
@@ -1,14 +1,30 @@
-import { Container } from '@mui/material';
+import { Box, Container, Grid, Typography } from '@mui/material';
 import React from 'react';
-import './MainPage.css';
-import Header from './components/Header';
-import Content from './components/Content';
-import Footer from './components/Footer';
+import Content from '../Components/Content';
+import Footer from '../Components/Footer';
+import Header from '../Components/Header';
 
 function MainPage() {
   return (
     <Container>
       <Header />
+      <Box>
+        <Grid
+          container
+          spacing={0}
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+          pt={20}
+          pb={10}
+        >
+          <Grid item xs={3}>
+            <Typography variant="h1" component="span">
+              The Rick and Morty API
+            </Typography>
+          </Grid>
+        </Grid>
+      </Box>
       <Content />
       <Footer />
     </Container>
