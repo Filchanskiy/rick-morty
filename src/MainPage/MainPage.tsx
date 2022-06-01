6ebfbde6f1f328b168f08e04211734ad3cb3bf28
Pavel <playwikki97@gmail.com>
2022-06-01 13:41:24 +0300

    Added a box with a picture

--- src/MainPage/MainPage.tsx
+++ src/MainPage/MainPage.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Grid, Typography } from '@mui/material';
+import { Box, CardMedia, Typography } from '@mui/material';
 import React from 'react';
 import Content from '../Components/Content';
 import Footer from '../Components/Footer';
@@ -6,28 +6,24 @@ import Header from '../Components/Header';
 
 function MainPage() {
   return (
-    <Container>
+    <>
       <Header />
-      <Box>
-        <Grid
-          container
-          spacing={0}
-          direction="column"
-          alignItems="center"
-          justifyContent="center"
-          pt={20}
-          pb={10}
-        >
-          <Grid item xs={3}>
-            <Typography variant="h1" component="span">
-              The Rick and Morty API
-            </Typography>
-          </Grid>
-        </Grid>
+      <Box bgcolor="black">
+        <Box sx={{ display: 'flex' }}>
+          <CardMedia
+            component="img"
+            sx={{ maxWidth: '100%', maxHeight: 550 }}
+            image="http://pristor.ru/wp-content/uploads/2018/08/%D0%9E%D1%82%D0%BB%D0%B8%D1%87%D0%BD%D1%8B%D0%B5-%D0%BA%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D0%BA%D0%B8-%D0%BD%D0%B0-%D1%80%D0%B0%D0%B1%D0%BE%D1%87%D0%B8%D0%B9-%D1%81%D1%82%D0%BE%D0%BB-%D0%B2-%D1%85%D0%BE%D1%80%D0%BE%D1%88%D0%B5%D0%BC-%D0%BA%D0%B0%D1%87%D0%B5%D1%81%D1%82%D0%B2%D0%B5-%D0%A0%D0%B8%D0%BA-%D0%B8-%D0%9C%D0%BE%D1%80%D1%82%D0%B8-1.jpeg"
+            alt="Rick and Morty"
+          />
+        </Box>
+        <Typography variant="h2" color="skyblue" textAlign="center">
+          The Rick and Morty API
+        </Typography>
       </Box>
       <Content />
       <Footer />
-    </Container>
+    </>
   );
 }
 export default MainPage;
